e510a2d8c107f570f95ee3746a0f9118a2ec2234
IsseHassan <cadeysey114@gmail.com>
2025-01-10 22:53:30 +0300

    Added cros

--- Backend/server.js
+++ Backend/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors(
   {
-    origin: ['http://localhost:5173', 'https://link-hub-api.vercel.app'],
+    origin: ['http://localhost:5173', 'https://new-linkhub.vercel.app'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE']
   }
